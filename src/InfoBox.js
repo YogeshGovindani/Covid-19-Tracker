eb546aecf9c726519e8f7ad2a86ed8f19a5ae9be
YogeshGovindani <50540663+YogeshGovindani@users.noreply.github.com>
2021-05-24 20:18:11 +0530

    Update InfoBox.js

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -8,7 +8,7 @@ function InfoBox(props) {
         <Card onClick={props.onClick} className={c} >
             <CardContent>
                 <Typography className="infoBox_title" color="textSecondary">{props.title}</Typography>
-                <h3 className="infoBox_cases" >{(props.cases / 1000).toFixed(1) + "k"}</h3>
+                <h3 className="infoBox_cases" >{(props.cases > 1000) ? (props.cases / 1000).toFixed(1) + "k" : props.cases}</h3>
                 <Typography className="infoBox_cases" color="textSecondary">{props.total} Total</Typography>
             </CardContent>
         </Card >
